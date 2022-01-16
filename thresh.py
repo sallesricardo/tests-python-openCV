7309fa1b167a7e87fdb1506f3eee23d2622589dd
Ricardo Salles <salles.ricardo@gmail.com>
2022-01-16 00:16:15 -0300

    Threshold image

---
+++ thresh.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import numpy as np
+
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+img = rescaleFrame(cv.imread('photos/car.jpg'), scale=0.25)
+cv.imshow('Car', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# Simple Thresholding
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+cv.imshow('Simple Threshold', thresh)
+
+threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
+cv.imshow('Simple Threshold Inverse', thresh_inv)
+
+# Adaptive Thresholding
+adaptive_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 3)
+cv.imshow('Adaptive Threshold', adaptive_thresh)
+
+
+cv.waitKey(0)
